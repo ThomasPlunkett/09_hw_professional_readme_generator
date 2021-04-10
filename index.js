f6d393ba08b0a60e4c64c8a9275d6334a5bc6b85
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-04-09 22:38:54 -0400

    working on second TODO in index.js file. Pseudocoding list of all the questions I need for the array for user input.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
+// What are the questions that I need?
+// GitHub username, Email, Project name, Short description of project, What kind of license, how to install, how to test, what do users need to know, and contributing.
 const questions = [];
 
 // TODO: Create a function to write README file
