a8298437586f3ad1ca25f11789a37beddb18d23e
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-04-09 22:54:12 -0400

    working on second TODO in index.js, I created an array for user input.

--- index.js
+++ index.js
@@ -5,7 +5,56 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 // What are the questions that I need?
 // GitHub username, Email, Project name, Short description of project, What kind of license, how to install, how to test, what do users need to know, and contributing.
-const questions = [];
+const questions = [
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your GitHub username?',
+      message: 'github?',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        message: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project name?',
+        message: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Hw would you describe your project?',
+        message: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What kind of license do you want?',
+        message: 'license',
+    },
+    {
+        type: 'input',
+        message: 'How is your project installed?',
+        message: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How is your project tested?',
+        message: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What do users need to know about your project?',
+        message: 'information',
+    },
+    {
+        type: 'input',
+        message: 'Who is contributing to your project?',
+        message: 'contributions',
+    },
+  ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
